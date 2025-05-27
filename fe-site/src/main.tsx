74e85a038a2fc91288e699310f1de5df47769a68
francescosommellaa <francescosommellaa@gmail.com>
2025-05-27 15:49:13 +0200

    chore: remove vercel analytics integration
    The analytics package was removed from both package.json and package-lock.json, and its usage was cleaned up from main.tsx. This change was made as the analytics functionality is no longer needed in the project.

--- fe-site/src/main.tsx
+++ fe-site/src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';  // Usa 'react-dom/client' invece di 'react-dom'
 import App from './App';
-import { Analytics } from "@vercel/analytics/next"
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <Analytics />
     <App />
   </React.StrictMode>
 );
